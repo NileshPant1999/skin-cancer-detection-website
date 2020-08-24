8e727f1cad3382df1586ac41c2dc4ee7ef7ab82b
Nilesh Pant <i.nilesh.pant@redcarpetup.com>
2020-08-24 22:45:02 +0530

    first commit

---
+++ website/src/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Header.css"
+
+function Header() {
+	return (
+		<div class="main">
+		   <nav>
+		      <ul class="navbar">
+		      <li>Home</li>
+		      <li>About</li>
+		      <li>Services</li>
+		      <li>Team</li>
+		      </ul>
+		   </nav>
+		</div>
+		)
+}
+
+export default Header
---
+++ website/src/Main.js
@@ -0,0 +1,24 @@
+import React from "react"
+import "./Main.css"
+
+function Main() {
+	return (
+		<div className="main_body">
+			<div className="main__left">
+				<div className="main__heading">
+					<h1 class="main__text">MedaCure AI</h1>
+				</div>
+				<div className="main__para">
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.
+			   </p>
+				</div>
+				<div className="main__button">
+					<button className="button1" type="button">Contact Us</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Main;
