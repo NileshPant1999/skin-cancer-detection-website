c9368272e09ba0eccc56f3a42be01d284cb3b380
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-27 02:05:03 +0530

    Updates

---
+++ precache-manifest.f2e0bb8154a5cbbdea85685b943ad1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d2a2a2341fdbf3179974de31456d8",
+    "url": "/skin-cancer-detection-website/index.html"
+  },
+  {
+    "revision": "49d562bc2b3b93615464",
+    "url": "/skin-cancer-detection-website/static/css/main.8ea7e965.chunk.css"
+  },
+  {
+    "revision": "ddef497940875ef8e885",
+    "url": "/skin-cancer-detection-website/static/js/2.deffb3e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skin-cancer-detection-website/static/js/2.deffb3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d562bc2b3b93615464",
+    "url": "/skin-cancer-detection-website/static/js/main.7fc96622.chunk.js"
+  },
+  {
+    "revision": "de6fe6f4b9ce7fa00055",
+    "url": "/skin-cancer-detection-website/static/js/runtime-main.5c7b4a7d.js"
+  },
+  {
+    "revision": "36effbef5f3561c12db8612f625e4583",
+    "url": "/skin-cancer-detection-website/static/media/web.36effbef.jpg"
+  }
+]);
